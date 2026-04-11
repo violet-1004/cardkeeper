57a4f5103429634ab2d39f747cbcbc4d2e48195a
Elma <cupcake.elma@gmail.com>
2026-04-12 02:50:18 +0800

    fix: update compatibility_date to auto-enable nodejs modules on Cloudflare

--- .wrangler/tmp/dev-qNMSHo/r01z3bor77k.js
+++ .wrangler/tmp/dev-qNMSHo/r01z3bor77k.js
@@ -1,7 +1,7 @@
 var __defProp = Object.defineProperty;
 var __name = (target, value) => __defProp(target, "name", { value, configurable: true });
 
-// .wrangler/tmp/bundle-UEOIlN/checked-fetch.js
+// .wrangler/tmp/bundle-LFjL7A/checked-fetch.js
 var urls = /* @__PURE__ */ new Set();
 function checkURL(request, init) {
   const url = request instanceof URL ? request : new URL(
@@ -936,7 +936,7 @@ var jsonError = /* @__PURE__ */ __name(async (request, env, _ctx, middlewareCtx)
 }, "jsonError");
 var middleware_miniflare3_json_error_default = jsonError;
 
-// .wrangler/tmp/bundle-UEOIlN/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-LFjL7A/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__ = [
   middleware_ensure_req_body_drained_default,
   middleware_miniflare3_json_error_default
@@ -968,7 +968,7 @@ function __facade_invoke__(request, env, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__, "__facade_invoke__");
 
-// .wrangler/tmp/bundle-UEOIlN/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-LFjL7A/middleware-loader.entry.ts
 var __Facade_ScheduledController__ = class ___Facade_ScheduledController__ {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
