7e7bef9d12790abd1d7ef36e17d8160861af7ea8
Elma <cupcake.elma@gmail.com>
2026-07-05 22:35:35 +0800

    0705 小卡api-1

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,6 +1,7 @@
 import AdminClient from './AdminClient';
 import { fetchSeriesAndGroups } from './actions';
 
+export const runtime = 'edge';
 export const dynamic = 'force-dynamic';
 export const revalidate = 0; // 🌟 確保伺服器端每次都重新抓取資料，絕對不使用舊快取
 
