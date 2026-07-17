88485c95f9c2b341e6c78cb3b4ef8c6e804e078f
Elma <cupcake.elma@gmail.com>
2026-07-17 12:45:43 +0800

    0716 小卡poca-1

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 // @ts-nocheck
 "use client";
 import Image from 'next/image';
+import { upsertPocaCards } from './admin/actions';
 import Link from 'next/link';
 import React, { useState, useMemo, useRef, useEffect, useCallback } from 'react';
 import { supabase as realSupabase } from '@/lib/supabaseClient'; // 🌟 保留原始連線供圖片上傳使用
